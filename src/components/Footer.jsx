058c07594e27b22e8fffbc22e1ceaacc35fcee96
Tooba <jtooba70@gmail.com>
2023-02-14 16:13:35 +0500

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+
+export default function Footer() {
+
+    return (
+      <footer>
+        <img src="../images/footer.png" alt="little lemon"/>
+        <div className="footer-content">
+            <div className="footer-nav">
+                <p>Navigtion</p>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Menu</a>
+                <a href="#">Reservations</a>
+                <a href="#">Order Online</a>
+                <a href="#">Login</a>
+            </div>
+            <div className="footer-contact">
+                <p>Contact</p>
+                <a href="#">Address</a>
+                <a href="#">Phone Number</a>
+                <a href="#">Email</a>
+            </div>
+            <div className="footer-social">
+                <p>Social Media</p>
+                <a href="#">Facebook</a>
+                <a href="#">Instagram</a>
+                <a href="#">TikTok</a>
+                <a href="#">Twitter</a>
+            </div>
+        </div>
+      </footer>
+    )
+  }
+  
+
+  
