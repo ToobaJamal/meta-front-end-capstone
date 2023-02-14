1db05cb60f00260f725de3a30b41f0dcc1fcd022
Tooba <jtooba70@gmail.com>
2023-02-14 15:52:57 +0500

    Add logo and Nav

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Nav from "./Nav"
+export default function Header() {
+
+    return (
+      <header>
+        <img src="../images/logo.png" alt="little lemon"/>
+        <Nav/>
+      </header>
+    )
+  }
+  
+
+  
