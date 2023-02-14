61d0fc5df996a7c2ce79e43b2a8c600c7c98023d
Tooba <jtooba70@gmail.com>
2023-02-14 15:53:17 +0500

    Add Nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+export default function Nav() {
+
+    return (
+      <nav>
+     
+        <a href="#">HOME</a>
+        <a href="#">ABOUT</a>
+        <a href="#">MENU</a>
+        <a href="#">RESERVATIONS</a>
+        <a href="#">ORDER ONLINE</a>
+        <a href="#">LOGIN</a>
+   
+      </nav>
+    )
+  }
+  
+
+  
