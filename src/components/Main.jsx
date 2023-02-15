dc80ad68d3a97c0946a12d64f0f4c62bd65fc4d3
Tooba <jtooba70@gmail.com>
2023-02-15 12:05:15 +0500

    Add components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,15 @@
+import Hero from "./Hero"
+import Menu from "./Menu"
+import Testimonials from "./Testimonials"
+import Description from "./Description"
 export default function Main() {
 
     return (
       <main>
-       
+       <Hero />
+       <Menu />
+       <Testimonials />
+       <Description />
       </main>
     )
   }
