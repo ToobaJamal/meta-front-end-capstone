a6953fc17d58bd39bc28f0df8c2e3697a369b4eb
Tooba <jtooba70@gmail.com>
2023-02-15 12:05:57 +0500

    Add Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+
+export default function Hero() {
+
+    return (
+      <div className="hero">
+        <div className="hero-content">
+            <div className="hero-desc">
+                <p className="title">Little Lemon</p>
+                <p className="subtitle">Chicago</p>
+                <p className="desc">We are a family owned Mediterranean restaurant, 
+                    focused on traditional recipes served with a modern twist.</p>
+                <button className="reserve-btn btn">
+                    Reserve a table
+                </button>
+            </div>
+            <div className="img-wrapper">
+            <img className="hero-img" src="../images/hero.png" alt="hero image"/>
+            </div>
+        </div>
+      </div>
+    )
+  }
+  
+
+  
