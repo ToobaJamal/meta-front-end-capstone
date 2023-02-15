84053a1b83d542b056937a479870d305228fc5c5
Tooba <jtooba70@gmail.com>
2023-02-15 12:06:34 +0500

    Add Testimonials component

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,50 @@
+
+export default function Testimonials() {
+
+    return (
+      <div className="testimonials-parent">
+        <p className="testimonials-title">Testimonials</p>
+        <div className="testimonials">
+            <div className="testimonial">
+                <img className="reviewer-img" src="../images/reviewer1.png" alt="greek salad"/>
+                <div className="name-rating">
+                    <p>John Doe</p>
+                    <p>5/5</p>
+                </div>
+                <div className="review-text">
+                    <p>The famous greek salad of crispy lettuce, 
+                        peppers, olives and our Chicago style feta cheese, 
+                        garnished with crunchy garlic and rosemary croutons. </p>
+                </div>
+            </div>
+            <div className="testimonial">
+                <img className="reviewer-img" src="../images/reviewer1.png" alt="greek salad"/>
+                <div className="name-rating">
+                    <p className="reviewer-name">John Doe</p>
+                    <p className="reviewer-rating">5/5</p>
+                </div>
+                <div className="review-text">
+                    <p>The famous greek salad of crispy lettuce, 
+                        peppers, olives and our Chicago style feta cheese, 
+                        garnished with crunchy garlic and rosemary croutons. </p>
+                </div>
+            </div>
+            <div className="testimonial">
+                <img className="reviewer-img" src="../images/reviewer1.png" alt="greek salad"/>
+                <div className="name-rating">
+                    <p>John Doe</p>
+                    <p>5/5</p>
+                </div>
+                <div className="review-text">
+                    <p>The famous greek salad of crispy lettuce, 
+                        peppers, olives and our Chicago style feta cheese, 
+                        garnished with crunchy garlic and rosemary croutons. </p>
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+  }
+  
+
+  
