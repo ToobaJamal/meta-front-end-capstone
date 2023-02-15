693e01e055a55208709d29efc29cbeda549c72c6
Tooba <jtooba70@gmail.com>
2023-02-15 12:04:00 +0500

    Add Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Main from './components/Main'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
      <Header />
+     <Main />
      <Footer />
     </>
   )
