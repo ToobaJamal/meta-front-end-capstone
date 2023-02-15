2eafc3329c2634057d67098c5ae9175200c1f98f
Tooba <jtooba70@gmail.com>
2023-02-15 14:13:19 +0500

    Add BookingPage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomePage from './components/HomePage'
+import HomePage from './pages/HomePage'
+import BookingPage from './pages/BookingPage'
 import './App.css'
 import {
   Route,
@@ -14,7 +15,7 @@ function App() {
      <Header />
      <Routes> 
       <Route path="/" exact element={<HomePage />}></Route>
-      {/* <Route path="/booking" element={<BookingPage />}></Route> */}
+      <Route path="/booking" element={<BookingPage />}></Route> 
     </Routes>
      <Footer />
     </>
