d17c49646230e4791e19e382826fae88883a45d5
Tooba <jtooba70@gmail.com>
2023-02-15 12:05:41 +0500

    Add Description component

---
+++ src/components/Description.jsx
@@ -0,0 +1,22 @@
+
+export default function Description() {
+
+    return (
+      <>
+        <div className="restaurant-desc-parent">
+            <div className="restaurant-desc">
+                <p className="title desc-title">Little Lemon</p>
+                <p className="subtitle desc-subtitle">Chicago</p>
+                <p className="desc-text desc">We are a family owned Mediterranean restaurant, 
+                    focused on traditional recipes served with a modern twist.</p>
+            </div>
+            
+            <img className="desc-img" src="../images/hero.png" alt="hero image"/>
+            
+        </div>
+      </>
+    )
+  }
+  
+
+  
