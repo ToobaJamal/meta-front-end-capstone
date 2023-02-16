d8dfcd276de5cbb8150f7fa003d7d1c5d1dcb5be
Tooba <jtooba70@gmail.com>
2023-02-16 19:13:47 +0500

    fetch time from api.js

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,9 +1,6 @@
 import { useState, useReducer } from "react"
 import BookingForm from "../components/BookingForm"
-
-
-  
-
+import { fetchAPI } from "../api"
   // function updateTimes(state, action) {
   //   switch (action.type) {
   //     case 'update': {
@@ -14,27 +11,43 @@ import BookingForm from "../components/BookingForm"
   //   }
   //   throw Error('Unknown action: ' + action.type);
   // }
+  console.log(fetchAPI(new Date("2018-12-31")))
 export default function BookingPage() {
-    const [availableTimes, dispatch] = useReducer(reducer, initializeTimes())
-   
+    
+    const [date, setDate] = useState(new Date())
+    // console.log(date.getDate())
+    
+
+    function initializeTimes(date) {
+      return fetchAPI(date)
+  //    return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+    }
+
     function updateTimes(date) {
     // TODO: update availableTimes based on the selected date
     // For now, we'll just return the same available times regardless of the date.
-    return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+    const dateObj = new Date(date)
+    return fetchAPI(dateObj)
+  
   }
 
-  function initializeTimes() {
-    return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-  }
+  console.log(updateTimes)
 
   function reducer(state, action) {
+    let newState
     switch (action.type) {
       case 'UPDATE_TIMES':
-        return updateTimes(action.payload);
+      const newDate = new Date(action.payload);
+      newState = updateTimes(newDate)
+      break;
+      
       default:
-        return state;
+        throw new Error()
     }
+    return newState
   }
+
+  const [availableTimes, dispatch] = useReducer(reducer, initializeTimes(date))
     return (
         <BookingForm availableTimes={availableTimes} dispatch={dispatch}/>
     )
