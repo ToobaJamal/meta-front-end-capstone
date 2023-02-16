ec7852531b2772e3497c97902c51c8982cd8ca4b
Tooba <jtooba70@gmail.com>
2023-02-16 21:02:44 +0500

    Add route for ConfirmedBooking

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
 import BookingPage from './pages/BookingPage'
+import ConfirmedBooking from './pages/ConfirmedBooking'
 import './App.css'
 import {
   Route,
@@ -16,6 +17,7 @@ function App() {
      <Routes> 
       <Route path="/" exact element={<HomePage />}></Route>
       <Route path="/booking" element={<BookingPage />}></Route> 
+      <Route path="/confirmed" element={<ConfirmedBooking />}></Route>
     </Routes>
      <Footer />
     </>
