3d415aaaf66e536c02d06b4fc00c780caa9e61ad
Tooba <jtooba70@gmail.com>
2023-02-16 12:23:01 +0500

    Add handleDateChange function

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,33 +1,42 @@
 import { useState } from "react"
-export default function BookingForm() {
+export default function BookingForm({availableTimes, dispatch}) {
     const [formData, setFormData] = useState({
         date: "",
-        time: "",
-        noOfGuests: 0,
-        occasion: ""
+        time: "17:00",
+        noOfGuests: 1,
+        occasion: "Birthday"
     })
-
-    console.log(formData)
-    function handleFormChange() {
-        
+    
+    const handleFormChange = (event) => {
+      const { name, value } = event.target
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [name]: value
+      }))
     }
+
+    const handleDateChange = (event) => {
+      const { name, value } = event.target
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [name]: value
+      }))
+      dispatch({ type: 'UPDATE_TIMES', payload: value })
+    }
+    console.log(formData)
+    const options = availableTimes.map(time => <option key={time}>{time}</option>)
     return (
         <form>
         <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" />
+        <input type="date" id="res-date" name="date" value={formData.date} onChange={handleDateChange}/>
         <label htmlFor="res-time">Choose time</label>
-        <select id="res-time ">
-           <option>17:00</option>
-           <option>18:00</option>
-           <option>19:00</option>
-           <option>20:00</option>
-           <option>21:00</option>
-           <option>22:00</option>
+        <select id="res-time " name="time" value={formData.time} onChange={handleFormChange}>
+           {options}
         </select>
         <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" />
+        <input type="number" placeholder="1" min="1" max="10" id="guests" name="noOfGuests" value={formData.noOfGuests} onChange={handleFormChange}/>
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion">
+        <select id="occasion" name="occasion" value={formData.occasion} onChange={handleFormChange}>
            <option>Birthday</option>
            <option>Anniversary</option>
         </select>
