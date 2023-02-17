a2c4cd3219adb6d3d74e204f6720bbb37150c098
Tooba <jtooba70@gmail.com>
2023-02-17 18:49:39 +0500

    Add content

--- src/pages/ConfirmedBooking.jsx
+++ src/pages/ConfirmedBooking.jsx
@@ -2,8 +2,10 @@
 export default function ConfirmedBooking() {
 
     return (
-      <main>
-       <h1>Booking Confirmed!!!</h1>
+      <main className="confirmed-main">
+       <p className="title confirmed-title">Booking Confirmed!!!</p>
+       <p className="subtitle confirmed-subtitle">Please provide your name and contact number at the reception upon arrival</p>
+       <p className="subtitle confirmed-subtitle">We are excited to serve you</p>
       </main>
     )
   }
