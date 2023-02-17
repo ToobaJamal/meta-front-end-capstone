6bab93e7b1b1503efd67f7962fbb7429400f9ef6
Tooba <jtooba70@gmail.com>
2023-02-17 20:06:27 +0500

    Add link to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import Nav from "./Nav"
+import { Link } from "react-router-dom"
 export default function Header() {
 
     return (
       <header>
-        <img src="../images/logo.png" alt="little lemon"/>
+        <Link to="/"><img src="../images/logo.png" alt="little lemon"/></Link>
         <Nav/>
       </header>
     )
