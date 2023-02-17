906c174a4b5483bd75e48d216cde7a9b72d68a47
Tooba <jtooba70@gmail.com>
2023-02-17 18:31:01 +0500

    Edit cards

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -26,30 +26,29 @@ export default function Menu() {
                 </div>
             </div>
             <div className="card">
-                <img className="menu-img" src="../images/greek-salad.png" alt="greek salad"/>
+                <img className="menu-img" src="../images/bruchetta.jpg" alt="bruchetta"/>
                 <div className="name-price">
-                    <p>Greek Salad</p>
-                    <p>$12.99</p>
+                    <p className="item-name">Bruchetta</p>
+                    <p className="item-price">$5.99</p>
                 </div>
                 <div className="item-desc">
-                    <p>The famous greek salad of crispy lettuce, 
-                        peppers, olives and our Chicago style feta cheese, 
-                        garnished with crunchy garlic and rosemary croutons. </p>
+                    <p>Our Bruschetta is made from grilled bread that has 
+                        been smeared with garlic and seasoned with salt and olive oil.  </p>
                 </div>
                 <div className="delivery">
                     <p>Order a delivery</p>
                 </div>
             </div>
             <div className="card">
-                <img className="menu-img" src="../images/greek-salad.png" alt="greek salad"/>
+                <img className="menu-img" src="../images/lemon-dessert.jpg" alt="lemon-dessert"/>
                 <div className="name-price">
-                    <p>Greek Salad</p>
-                    <p>$12.99</p>
+                    <p className="item-name">Lemon Dessert</p>
+                    <p className="item-price">$5.00</p>
                 </div>
                 <div className="item-desc">
-                    <p>The famous greek salad of crispy lettuce, 
-                        peppers, olives and our Chicago style feta cheese, 
-                        garnished with crunchy garlic and rosemary croutons. </p>
+                    <p>This comes straight from grandma’s recipe book,
+                         every last ingredient has been sourced and is as 
+                         authentic as can be imagined. </p>
                 </div>
                 <div className="delivery">
                     <p>Order a delivery</p>
