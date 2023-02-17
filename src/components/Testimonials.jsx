5cefa0aeebd825b352808db775f1ee76f9649daa
Tooba <jtooba70@gmail.com>
2023-02-17 18:31:21 +0500

    Edit testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,37 +8,37 @@ export default function Testimonials() {
             <div className="testimonial">
                 <img className="reviewer-img" src="../images/reviewer1.png" alt="greek salad"/>
                 <div className="name-rating">
-                    <p>John Doe</p>
-                    <p>5/5</p>
+                    <p className="reviewer-name">John Doe</p>
+                    <p className="reviewer-rating">5/5</p>
                 </div>
                 <div className="review-text">
-                    <p>The famous greek salad of crispy lettuce, 
-                        peppers, olives and our Chicago style feta cheese, 
-                        garnished with crunchy garlic and rosemary croutons. </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa, 
+                        semper sed enim at, faucibus mattis lorem. Donec vehicula, leo ut auctor ullamcorper, 
+                        diam tortor dignissim velit, ac iaculis libero lacus non diam. </p>
                 </div>
             </div>
             <div className="testimonial">
-                <img className="reviewer-img" src="../images/reviewer1.png" alt="greek salad"/>
+                <img className="reviewer-img" src="../images/reviewer2.png" alt="greek salad"/>
                 <div className="name-rating">
                     <p className="reviewer-name">John Doe</p>
                     <p className="reviewer-rating">5/5</p>
                 </div>
                 <div className="review-text">
-                    <p>The famous greek salad of crispy lettuce, 
-                        peppers, olives and our Chicago style feta cheese, 
-                        garnished with crunchy garlic and rosemary croutons. </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa, 
+                        semper sed enim at, faucibus mattis lorem. Donec vehicula, leo ut auctor ullamcorper, 
+                        diam tortor dignissim velit, ac iaculis libero lacus non diam. </p>
                 </div>
             </div>
             <div className="testimonial">
-                <img className="reviewer-img" src="../images/reviewer1.png" alt="greek salad"/>
+                <img className="reviewer-img" src="../images/reviewer3.png" alt="greek salad"/>
                 <div className="name-rating">
-                    <p>John Doe</p>
-                    <p>5/5</p>
+                    <p className="reviewer-name">John Doe</p>
+                    <p className="reviewer-rating">5/5</p>
                 </div>
                 <div className="review-text">
-                    <p>The famous greek salad of crispy lettuce, 
-                        peppers, olives and our Chicago style feta cheese, 
-                        garnished with crunchy garlic and rosemary croutons. </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa, 
+                        semper sed enim at, faucibus mattis lorem. Donec vehicula, leo ut auctor ullamcorper, 
+                        diam tortor dignissim velit, ac iaculis libero lacus non diam. </p>
                 </div>
             </div>
         </div>
