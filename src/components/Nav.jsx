b0c71e002e6999311d23120811493faa7e3ef2da
Tooba <jtooba70@gmail.com>
2023-02-17 20:07:01 +0500

    Add toggle nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,19 @@
 import { Outlet, Link } from "react-router-dom"
+import { useState } from 'react';
+import { FaRegTimesCircle, FaGripLines } from 'react-icons/fa';
 
 export default function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
     return (
-      <nav>
-     
+      <>
+      {isOpen ? <FaRegTimesCircle className="mobile-nav" onClick={toggleNav}/> : <FaGripLines className="mobile-nav" onClick={toggleNav}/>}
+      {/* <div className="mobile-nav" onClick={toggleNav}>X</div> */}
+      <nav className={`nav ${isOpen ? 'active' : ''}`} onClick={toggleNav}>
+        
         <Link to="/" className="nav-item">HOME</Link>
         <a href="#" className="nav-item">ABOUT</a>
         <a href="#" className="nav-item">MENU</a>
@@ -13,6 +22,7 @@ export default function Nav() {
         <a href="#" className="nav-item">LOGIN</a>
    
       </nav>
+      </>
     )
   }
   
