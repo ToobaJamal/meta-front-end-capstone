df48abefaa99e272597ef6532378cbdf64dbb395
Tooba <jtooba70@gmail.com>
2023-02-18 14:14:50 +0500

    Format code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import Menu from "../components/Menu"
 import Testimonials from "../components/Testimonials"
 import Description from "../components/Description"
 export default function HomePage() {
-
     return (
       <main>
        <Hero />
